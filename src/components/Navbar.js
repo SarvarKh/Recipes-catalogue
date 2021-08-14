0ae1e5ce8a95f491c20357fe52767dd55d21345a
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-14 15:49:16 +0500

    Add routers and implement navigation

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/components/Detail">Details</Link>
+    </div>
+  );
+}
---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import App from './containers/App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Detail from './containers/Detail';
+import Error from './components/Error';
+import Navbar from './components/Navbar';
+import * as styles from './style/components.module.css';
+
+const Routes = () => (
+  <BrowserRouter>
+    <div className={styles.navContainer}>
+      <h1 className={styles.logo}>Inspiring Recipes Catalogue</h1>
+      <Navbar />
+    </div>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/components/App" component={Detail} />
+      <Route component={Error} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
+
