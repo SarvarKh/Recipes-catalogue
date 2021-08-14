866aa2c316d46ffe89d51081acaad5241351b9c0
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-14 18:49:28 +0500

    Fetch category and display via select with option tags

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,16 +1,32 @@
+import { indexOf } from "lodash";
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [meals, setMeals] = useState(["Not fetched yet"]);
+  const [meals, setMeals] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
     .then(res => res.json())
     .then(data => setMeals(data.meals));
+
+    fetch("https://www.themealdb.com/api/json/v1/1/list.php?c=list")
+    .then(res => res.json())
+    .then(data => setCategories(data.meals));
   }, [])
 
     return (
       <div>
+        <h1>Categories</h1>
+          <select className="nav-item appearance-none cursor-pointer">
+            {categories.map(category => (
+              <option key={indexOf(category)} value={category.strCategory}>
+                {category.strCategory}
+              </option>
+            ))}
+          </select>
+
+
         <h1>Meals</h1>
         {
           meals.map(meal => (
