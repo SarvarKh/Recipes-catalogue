e848498f6e9d341f018db39b823b842e1d2be71e
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-15 11:25:17 +0500

    LAdd created redux store to Provider component as prop

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import store from './store'
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <React.StrictMode>
       <Routes />
     </React.StrictMode>
