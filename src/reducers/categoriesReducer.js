e7404a50dd19087898ea58fbc025b5e94990b5c9
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-15 11:04:21 +0500

    Add categories Reducer

---
+++ src/reducers/categoriesReducer.js
@@ -0,0 +1,13 @@
+import { FETCH_CATEGORIES } from '../actions/types';
+
+const categoriesReducer = (state = [], action) => {
+    switch (action.type) {
+        case FETCH_CATEGORIES:
+            return action.categories
+    
+        default:
+            return state;
+    }
+}
+
+export default categoriesReducer;
