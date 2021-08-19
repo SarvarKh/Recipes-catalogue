dcf3bef1dad3f08ca0645f7be2ccb01bcc5932e2
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-19 14:25:46 +0500

    Style navbar

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,16 @@
 import App from './components/App';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Detail from './containers/Detail';
 import DetailMeal from './components/DetailMeal';
 import Error from './components/Error';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import * as styles from './style/components.module.css';
 import { Link } from "react-router-dom";
 
 const Routes = () => (
   <BrowserRouter>
-    <div className={styles.navContainer}>
-      <Link to="/" className={styles.logo}></Link>
+    <div className="navbar">
+      <Link to="/" className="logo"></Link>
+      <h2 className="quote">Good food is good mood</h2>
       <Navbar />
     </div>
     <Switch>
