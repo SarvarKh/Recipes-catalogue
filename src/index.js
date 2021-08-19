5b8944b645fabba79540810d53f5fe5ecb17149b
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-19 19:45:00 +0500

    Style mobile category and search parts

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 import './style/index.css';
+import './style/small_screens.css';
 import { Provider } from 'react-redux';
-import store from './store'
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
