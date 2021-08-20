8076b91cffffaadc17f385bd4275f0df53e58ca1
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-20 11:24:50 +0500

    Fix Footer.js linter errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,15 @@ export default function Footer() {
       <footer>
         <div>
           Copyright © 2021 Sarvar Khalimov
-          <a className="icon-link" href="https://github.com/SarvarKh" target="_blank" rel="noreferrer"><i className="fab fa-github" aria-hidden="true" /></a>
-          <a className="icon-link" href="https://www.linkedin.com/in/sarvar-khalimov/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin" aria-hidden="true" /></a>
-          <a className="icon-link" href="https://sarvarkh.github.io/portfolio/" target="_blank" rel="noreferrer"><i className="fas fa-folder-open" aria-hidden="true" /></a>
+          <a className="icon-link" href="https://github.com/SarvarKh" target="_blank" rel="noreferrer">
+            <i className="fab fa-github" aria-hidden="true" />
+          </a>
+          <a className="icon-link" href="https://www.linkedin.com/in/sarvar-khalimov/" target="_blank" rel="noreferrer">
+            <i className="fab fa-linkedin" aria-hidden="true" />
+          </a>
+          <a className="icon-link" href="https://sarvarkh.github.io/portfolio/" target="_blank" rel="noreferrer">
+            <i className="fas fa-folder-open" aria-hidden="true" />
+          </a>
         </div>
       </footer>
     </>
