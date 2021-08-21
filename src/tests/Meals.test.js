acc794eec4c90669808bde195045dd7e6257a09e
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-21 17:01:02 +0500

    Update snapshot test for Meals.js

--- src/tests/Meals.test.js
+++ src/tests/Meals.test.js
@@ -3,7 +3,18 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import Meals from '../components/presentation/Meals';
 
-const meals = undefined;
+const meals = [
+  {
+    strMeal: 'Apam balik',
+    strMealThumb: 'https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg',
+    idMeal: '53049'
+  },
+  {
+    strMeal: 'Apple & Blackberry Crumble',
+    strMealThumb: 'https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg',
+    idMeal: '52893'
+  },
+];
 
 const clickOnDetailMeal = () => (
     console.log('Test Meals and logging Meals')
