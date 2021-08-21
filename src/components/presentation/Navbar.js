abd2f323d847491e22b5716d9949e87beebca550
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-21 12:45:40 +0500

    Add snapshot test for Navbar.js

--- src/components/presentation/Navbar.js
+++ src/components/presentation/Navbar.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <div>
-      <Link to="/">Home</Link>
+      <a href="/">Home</a>
     </div>
   );
 }
