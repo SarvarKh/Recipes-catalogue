0229afdd257aa09b615935540c3351e83b6dc5ef
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-21 15:48:08 +0500

    Write unit tests for categoriesReducer.js

--- src/tests/testingReducers/categoriesReducer.test.js
+++ src/tests/testingReducers/categoriesReducer.test.js
@@ -3,3 +3,22 @@ import categoriesReducer from '../../reducers/categoriesReducer';
 test('should return the initial state', () => {
   expect(categoriesReducer(undefined, {})).toEqual([])
 })
+
+test('should add a category redux store', () => {
+  const previousState = {
+    detailMeal: {},
+    meals: [],
+    categories: [],
+    searchedItems: [],
+  };
+  const action = {
+    type: 'FETCH_CATEGORIES',
+    payload: 'New category',
+  }
+  expect(categoriesReducer(previousState, action)).toEqual({
+    detailMeal: {},
+    meals: [],
+    categories: 'New category',
+    searchedItems: [],
+  })
+})
