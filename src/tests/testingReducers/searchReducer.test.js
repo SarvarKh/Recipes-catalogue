1eb169a73f3d98d1ec46fdae1b2f2173e7997256
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-21 16:27:03 +0500

    Write unit tests for meals and search Reducers

--- src/tests/testingReducers/searchReducer.test.js
+++ src/tests/testingReducers/searchReducer.test.js
@@ -3,3 +3,22 @@ import searchReducer from '../../reducers/searchReducer';
 test('should return the initial state', () => {
   expect(searchReducer(undefined, {})).toEqual([])
 })
+
+test('should search meals and add that meals arr to redux store', () => {
+  const previousState = {
+    detailMeal: {},
+    meals: [],
+    categories: [],
+    searchedItems: [],
+  };
+  const action = {
+    type: 'FETCH_BY_NAME',
+    payload: ["Meal 01", "Meal 01", "Bunch of meals"],
+  }
+  expect(searchReducer(previousState, action)).toEqual({
+    detailMeal: {},
+    meals: [],
+    categories: [],
+    searchedItems: ["Meal 01", "Meal 01", "Bunch of meals"],
+  })
+})
