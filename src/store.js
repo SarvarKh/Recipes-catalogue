17a35489484ec1bc2689b9f01f1d9af864887ef2
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-21 11:46:38 +0500

    Install redux dev tools npm pkg and set it up

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers/rootReducer';
 
 const initialState = {
@@ -14,7 +15,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
+  composeWithDevTools(
     applyMiddleware(...middleware),
   ),
 );
