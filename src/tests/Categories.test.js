66009961182e9c55dc343050bfbc20e25f875a4f
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-21 16:52:48 +0500

    Update snapshot test for Categories.js

--- src/tests/Categories.test.js
+++ src/tests/Categories.test.js
@@ -3,7 +3,20 @@ import renderer from 'react-test-renderer';
 import { BrowserRouter } from 'react-router-dom';
 import Categories from '../components/presentation/Categories';
 
-const categories = undefined;
+const categories = [
+  {
+    idCategory: "1",
+    strCategory: "Beef",
+    strCategoryThumb: "https://www.themealdb.com/images/category/beef.png",
+    strCategoryDescription: "Beef is the culinary name for meat from cattle",
+  },
+  {
+    idCategory: "2",
+    strCategory: "Lamb",
+    strCategoryThumb: "https://www.themealdb.com/images/category/beef.png",
+    strCategoryDescription: "Lamb is ...",
+  },
+];
 
 const handleClick = () => (
     console.log('Test Categories and logging categories')
