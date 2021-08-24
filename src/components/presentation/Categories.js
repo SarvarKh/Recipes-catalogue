c77325a703f6c1c5ef21d4a307d773624a313b1e
SarvarKhalimov <khalimovsarvar@gmail.com>
2021-08-24 21:36:57 +0500

    Move MapCategories.js to container component directory

--- src/components/presentation/Categories.js
+++ src/components/presentation/Categories.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import MapCategories from './MapCategories';
+import MapCategories from '../container/MapCategories';
 
 const Categories = ({ handleClick }) => (
   <>
